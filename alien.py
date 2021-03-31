51af88da98347958c7327201dd84e8f3c98c3c0a
Roulio359 <jules-lvd@hotmail.fr>
2021-03-31 08:38:05 +0200

    1

--- alien.py
+++ alien.py
@@ -3,9 +3,9 @@ from pygame import Vector2
 
 class alien:
     def __init__(self):
-        self.model1 = pygame.image.load('.png')
-        self.model2 = pygame.image.load('.png')
-        self.model3 = pygame.image.load('.png')
+        self.model1 = pygame.image.load('Model\big.gif')
+        self.model2 = pygame.image.load('Model\med.gif')
+        self.model3 = pygame.image.load('Model\sma.gif')
         self.vitesse = 30
         self.position = Vector2()
 
